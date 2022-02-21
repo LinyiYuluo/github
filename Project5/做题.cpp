34beaea8612cd58bcba6377ad58b466cf5691a56
LinyiYuluo <3180834411@qq.com>
2022-02-21 16:23:26 +0800

    2.20做题代码

---
+++ Project5/做题.cpp
@@ -0,0 +1,95 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#include<math.h>
+//
+
+//计算圆的数据
+
+//int main()
+//{
+//	double i = 0;
+//	scanf("%lf", &i);
+//	double pi = 3.14159;
+//	printf("%.4lf %.4lf %.4lf", 2.0 * i, 2.0 * i * pi, i * pi * i);
+//	return 0;
+//}
+//int main()
+//{
+//	float r1 = 0;
+//	float r2 = 0;
+//	scanf("%f %f" ,&r1, &r2);
+//	float R = (float)1 / (r1 + r2);
+//	printf("%.2f", R);
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	unsigned int n = 0;
+//	scanf("%d", &n);
+//	int sum = 1;
+//	for (int i = 1; i <= n; i++)
+//	{
+//		sum = 2 * sum;
+//	}
+//	printf("%d", sum);
+//	return 0;
+//}
+
+//输出两个整数的乘积，注意乘积的类型应选用long
+
+//#include<stdio.h>
+//int main()
+//{
+//	unsigned int A, B;
+//	scanf("%d %d", &A, &B);
+//	while (A >= 1 && B <= 50000)
+//	{
+//		printf("%d", A * B);
+//		break;
+//	}
+//	return 0;
+//}
+
+
+//反向输入一个三位数
+//
+//
+//#include<stdio.h>
+//int main()
+//{
+//	unsigned int i = 0;
+//	scanf("%d", &i);
+//	while (i < 1000)
+//	{
+//		while (i)
+//		{
+//			int n = i % 10;
+//			printf("%d", n);
+//			i = i / 10;
+//		}
+//		break;
+//	}
+//	return 0;
+//}
+
+
+
+//输出一个实数，求球的体积,注意double类型数据输入时应增加小数点后一位，不然计算结果会出错
+
+//#include<stdio.h>
+//int main()
+//{
+//    double r=0;
+//	scanf("%lf", &r);
+//	while (r > 0 && r <= 100)
+//	{
+//		double v = (double)((4.0/ 3) * (3.14 * r * r * r));
+//		printf("%.2lf", v);
+//		break;
+//	}
+//	return 0;
+//}
+
